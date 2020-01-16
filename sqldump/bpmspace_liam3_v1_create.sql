49c3b5841718466f6bf06284270cd18804885bad
rootmessages BPMspace <5494549+BPMspace@users.noreply.github.com>
2020-01-16 10:58:01 +0100

    Update bpmspace_liam3_v1_create.sql

--- sqldump/bpmspace_liam3_v1_create.sql
+++ sqldump/bpmspace_liam3_v1_create.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE bpmspace_liam3_v1;
 CREATE USER 'bpmspace_liam3_v1'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON bpmspace_liam3_v1 TO 'bpmspace_liam3_v1'@'localhost';
+GRANT CREATE, INSERT , SELECT , UPDATE ON bpmspace_liam3_v1.* TO 'bpmspace_liam3_v1'@'localhost';
+FLUSH PRIVILEGES;
 USE bpmspace_liam3_v1;
