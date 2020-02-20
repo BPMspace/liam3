077cf843550095a5c556646af6956b324e1338ae
Robert Kuhlig <16413104+Kuhlig@users.noreply.github.com>
2020-02-20 08:40:24 +0100

    Update import_sql_all.sh

--- sqldump/import_sql_all.sh
+++ sqldump/import_sql_all.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-mysql -u root -p < ./bpmspace_liam3_v1_create.sql
 mysql -u bpmspace_liam3_v1 -p bpmspace_liam3_v1 < ./bpmspace_liam3_v1_structur.sql
 mysql -u bpmspace_liam3_v1 -p bpmspace_liam3_v1 < ./bpmspace_liam3_v1_statemachine.sql
 mysql -u bpmspace_liam3_v1 -p bpmspace_liam3_v1 < ./bpmspace_liam3_v1_min.sql 
