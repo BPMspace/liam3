46a73d975acf1a37101f6990a7674239d04a6d90
Robert Kuhlig <16413104+Kuhlig@users.noreply.github.com>
2020-02-24 19:46:45 +0100

    Update import_sql_all.sh

--- sqldump/import_sql_all.sh
+++ sqldump/import_sql_all.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+touch /tmp/sqlimport1_.log
 mysql -u root -p$DB_ROOT_PASSWORD -h $DB_HOST $DB_NAME < /var/www/html/sqldump/bpmspace_liam3_v1_structure.sql
 mysql -u root -p$DB_ROOT_PASSWORD -h $DB_HOST $DB_NAME < /var/www/html/sqldump/bpmspace_liam3_v1_statemachine.sql
 mysql -u root -p$DB_ROOT_PASSWORD -h $DB_HOST $DB_NAME < /var/www/html/sqldump/bpmspace_liam3_v1_min.sql 
 mysql -u root -p$DB_ROOT_PASSWORD -h $DB_HOST $DB_NAME < /var/www/html/sqldump/bpmspace_liam3_v1_diff.sql
+touch /tmp/sqlimport2_.log
