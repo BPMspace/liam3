0309d5ae2a56d62b27aa913f435874ee30135abe
Robert Kuhlig <16413104+Kuhlig@users.noreply.github.com>
2020-02-27 16:47:51 +0100

    new sh structure

---
+++ scripts/liam3-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+/usr/local/bin/production_server_master.sh
+/usr/local/bin/liam3-import_sql_all.sh
+
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+        set -- apache2-foreground "$@"
+fi
+
+exec "$@"
