77873c37056e30fc17d9b40e4322e2a12465d9c8
Robert Kuhlig <16413104+Kuhlig@users.noreply.github.com>
2020-02-28 11:30:41 +0100

    bugix

--- scripts/liam3-entrypoint.sh
+++ scripts/liam3-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 /usr/local/bin/production_server_master.sh
-#/usr/local/bin/liam3-import_sql_all.sh
+/usr/local/bin/liam3-import_sql_all.sh
 
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
