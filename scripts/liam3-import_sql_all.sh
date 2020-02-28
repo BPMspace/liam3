05bc185fe683788681b44c1db3d4ea69d38992b4
Robert Kuhlig <16413104+Kuhlig@users.noreply.github.com>
2020-02-28 12:47:40 +0100

    Update liam3-import_sql_all.sh

--- scripts/liam3-import_sql_all.sh
+++ scripts/liam3-import_sql_all.sh
@@ -2,17 +2,17 @@
 for i in {1..50}
 do
 # check if db is up 
-mysql -u root -p$DB_ROOT_PASSWORD -h $DB_HOST $DB_NAME -e "use bpmspace_liam3-v1;"
-	if [ $? -eq 0 ]
-	then
-		mysql -u root -p$DB_ROOT_PASSWORD -h $DB_HOST $DB_NAME < /var/www/html/sqldump/bpmspace_liam3_v1_structure.sql
-		mysql -u root -p$DB_ROOT_PASSWORD -h $DB_HOST $DB_NAME < /var/www/html/sqldump/bpmspace_liam3_v1_statemachine.sql
-		mysql -u root -p$DB_ROOT_PASSWORD -h $DB_HOST $DB_NAME < /var/www/html/sqldump/bpmspace_liam3_v1_min.sql 
-		mysql -u root -p$DB_ROOT_PASSWORD -h $DB_HOST $DB_NAME < /var/www/html/sqldump/bpmspace_liam3_v1_diff.sql
-	  exit 0
-	else
-		sleep 5
-	fi
+	mysql -u root -p$DB_ROOT_PASSWORD -h $DB_HOST $DB_NAME -e "use bpmspace_liam3-v1;"
+		if [ $? -eq 0 ]
+		then
+			mysql -u root -p$DB_ROOT_PASSWORD -h $DB_HOST $DB_NAME < /var/www/html/sqldump/bpmspace_liam3_v1_structure.sql
+			mysql -u root -p$DB_ROOT_PASSWORD -h $DB_HOST $DB_NAME < /var/www/html/sqldump/bpmspace_liam3_v1_statemachine.sql
+			mysql -u root -p$DB_ROOT_PASSWORD -h $DB_HOST $DB_NAME < /var/www/html/sqldump/bpmspace_liam3_v1_min.sql 
+			mysql -u root -p$DB_ROOT_PASSWORD -h $DB_HOST $DB_NAME < /var/www/html/sqldump/bpmspace_liam3_v1_diff.sql
+		  exit 0
+		else
+			sleep 5
+		fi
 done
 echo "Failure: Could not connect to $DB_HOST $DB_NAME" >&2
 exit 1
